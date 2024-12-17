23d60dc9e0d5ea13aaa68fb00fc682d5f57ebeb5
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-17 18:19:40 +0200

    Add Position model
    - Field 'name' with unique & length constraints
    
    - Set ordering by name
    
    - Override save to call full_clean
    
    - Override __str__ to return name

--- task_manager/models.py
+++ task_manager/models.py
@@ -13,3 +13,17 @@ class TaskType(models.Model):
 
 	def __str__(self) -> str:
 		return self.name
+
+
+class Position(models.Model):
+	name = models.CharField(max_length=100, unique=True)
+
+	class Meta:
+		ordering = ["name"]
+
+	def save(self, *args, **kwargs):
+		self.full_clean()
+		super().save(*args, **kwargs)
+
+	def __str__(self) -> str:
+		return self.name
