ee6d013f9507f4f312d33cf78fbe96b6e3938aea
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-17 18:14:24 +0200

    Set INTERNAL_IPS & ALLOWED_HOSTS in settings.py

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+INTERNAL_IPS = ["127.0.0.1"]
+
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 # Application definition
 
