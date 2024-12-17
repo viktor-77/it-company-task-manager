7e94c2c39be07bc350bcb225d0ef491791d80c9e
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-18 00:37:19 +0200

    Register Worker model in admin
    - Add position to list_display, list_editable, list_filter,
     fieldsets & add_fieldsets
    
    - Add username, first_name & last_name to search_fields

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from task_manager.models import Worker
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+	list_display = UserAdmin.list_display + ("position",)
+	list_editable = ["position"]
+	list_filter = ("position",)
+	search_fields = ("username", "first_name", "last_name")
+
+	fieldsets = UserAdmin.fieldsets + (
+		(("Position", {
+			"classes": ("wide", "collapse",),
+			"fields": ("position",),
+		}),)
+	)
+
+	add_fieldsets = UserAdmin.add_fieldsets + (
+		(
+			(
+				"Details",
+				{
+					"fields": (
+						"position",
+						"first_name",
+						"last_name",
+					)
+				},
+			),
+		)
+	)
