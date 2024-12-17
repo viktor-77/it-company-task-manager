85a7511299588732f1b5779bc67908ba68fd0c54
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-17 18:20:53 +0200

    Add Worker model
    - Model extends from AbstractUser
    
    - Field 'position' is ForeignKey with SET_NULL on delete
    
    - Set verbose names for admin display
    
    - Override __str__ to return username

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -27,3 +28,19 @@ class Position(models.Model):
 
 	def __str__(self) -> str:
 		return self.name
+
+
+class Worker(AbstractUser):
+	position = models.ForeignKey(
+		Position,
+		on_delete=models.SET_NULL,
+		null=True,
+		related_name="workers",
+	)
+
+	class Meta(AbstractUser.Meta):
+		verbose_name = "Worker"
+		verbose_name_plural = "Workers"
+
+	def __str__(self) -> str:
+		return self.username
