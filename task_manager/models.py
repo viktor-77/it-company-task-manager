50bf8eeb6e26d3732ff3196e3bb045da5f2f15da
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-17 18:19:09 +0200

    Add TaskType model
    - Field 'name' with unique & length constraints
    
    - Set ordering by name
    
    - Override save to call full_clean
    
    - Override __str__ to return name

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+	name = models.CharField(max_length=100, unique=True)
+
+	class Meta:
+		ordering = ["name"]
+
+	def save(self, *args, **kwargs):
+		self.full_clean()
+		super().save(*args, **kwargs)
+
+	def __str__(self) -> str:
+		return self.name
