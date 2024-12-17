7e1beb9ee2aff30a8b15aa0ac987f5ddb373c186
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-17 18:25:00 +0200

    Add Task model
    Fields: name, description, created_at, deadline,
    is_complete, priority, task_type, assignees

--- task_manager/models.py
+++ task_manager/models.py
@@ -44,3 +44,27 @@ class Worker(AbstractUser):
 
 	def __str__(self) -> str:
 		return self.username
+
+
+class Task(models.Model):
+	DEADLINE_ERROR_MESSAGE = "The deadline cannot be in the past."
+	PRIORITY_CHOICES = [
+		(4, "Urgent"),
+		(3, "High"),
+		(2, "Medium"),
+		(1, "Low"),
+	]
+
+	name = models.CharField(max_length=100, unique=True, db_index=True)
+	description = models.TextField()
+	created_at = models.DateField(auto_now_add=True)
+	deadline = models.DateField()
+	is_completed = models.BooleanField(default=False)
+	priority = models.IntegerField(choices=PRIORITY_CHOICES)
+	task_type = models.ForeignKey(
+		TaskType,
+		on_delete=models.SET_NULL,
+		null=True,
+		related_name="tasks"
+	)
+	assignees = models.ManyToManyField(Worker, related_name="tasks")
