4e1dcfc0795b2abf766ad59fc2cde6e038e9f1ab
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-18 15:51:25 +0200

    Add index view
    - Add total task count, active task count & user count to context
    
    - Render data to pages/index.html template

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Count, Q
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Task
+
+
+def index(request: HttpRequest) -> HttpResponse:
+	task_counts = Task.objects.aggregate(
+		total_tasks=Count("id"),
+		active_tasks=Count("id", filter=Q(is_completed=False))
+	)
+
+	return render(
+		request,
+		"pages/index.html",
+		{
+			"total_tasks": task_counts["total_tasks"],
+			"active_tasks": task_counts["active_tasks"],
+			"total_users": get_user_model().objects.count(),
+		}
+	)
