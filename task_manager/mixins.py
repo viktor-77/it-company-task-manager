b27cec175fed3539400949de9214d68568ca98e4
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-19 23:38:07 +0200

    Add SearchMixin to include search form in context
    Mixin overrides get_context_data to add search form with initial
    data to the context

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -1,8 +1,25 @@
+from task_manager.forms import SearchForm
+
+
 class PreviousPageMixin:
 	"""Adds previous page url to the context."""
 
 	def get_context_data(self, **kwargs) -> dict:
 		context = super().get_context_data(**kwargs)
 		context["previous_page"] = self.request.META.get("HTTP_REFERER", "/")
+
+		return context
+
+
+class SearchMixin:
+	"""Adds a search form with the query parameter to the context."""
+
+	def get_context_data(self, **kwargs) -> dict:
+		context = super().get_context_data(**kwargs)
+		search_query = str(self.request.GET.get("query", "")).strip()
+
+		context["search_form"] = SearchForm(
+			initial={"query": search_query}
+		)
 
 		return context
