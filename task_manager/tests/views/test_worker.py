6ef6f0f64aec25893fd05035067605bf13985b6d
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-19 18:14:02 +0200

    Add tests for worker detail page
    Test context data, login requirement & authenticated user access

--- task_manager/tests/views/test_worker.py
+++ task_manager/tests/views/test_worker.py
@@ -1,11 +1,15 @@
+from datetime import date
+
+from django.conf.global_settings import LOGIN_URL
 from django.test import TestCase
 from django.urls import reverse
 
 from task_manager.tests.utils import (
-	create_worker,
+	create_task, create_worker,
 )
 
 WORKER_LIST_URL = "task_manager:worker_list"
+WORKER_DETAIL_URL = "task_manager:worker_detail"
 
 
 class WorkerListViewTest(TestCase):
@@ -69,3 +73,62 @@ class WorkerListViewTest(TestCase):
 		self.assertEqual(
 			len(response.context["worker_list"]), 2
 		)
+
+
+class WorkerDetailViewTest(TestCase):
+	@classmethod
+	def setUpTestData(cls) -> None:
+		cls.user = create_worker()
+		cls.active_task1 = create_task(
+			name="Active Task 1", is_completed=False
+		)
+		cls.active_task2 = create_task(
+			name="Active Task 2", is_completed=False
+		)
+		cls.resolved_task1 = create_task(
+			name="Resolved Task1", is_completed=True
+		)
+		cls.resolved_task2 = create_task(
+			name="Resolved Task2", is_completed=True
+		)
+
+		cls.active_task1.assignees.add(cls.user)
+		cls.active_task2.assignees.add(cls.user)
+		cls.resolved_task1.assignees.add(cls.user)
+		cls.resolved_task2.assignees.add(cls.user)
+
+	def test_worker_detail_view_login_required(self) -> None:
+		response = self.client.get(
+			reverse(WORKER_DETAIL_URL, args=[self.user.pk])
+		)
+
+		self.assertEqual(response.status_code, 302)
+		self.assertIn(LOGIN_URL, response.url)
+
+	def test_worker_detail_view_accessible_for_authenticated_users(
+		self
+	) -> None:
+		self.client.force_login(self.user)
+		response = self.client.get(
+			reverse(WORKER_DETAIL_URL, args=[self.user.pk])
+		)
+
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, "pages/worker_detail.html")
+
+	def test_worker_detail_view_context_data(self) -> None:
+		self.client.force_login(self.user)
+		response = self.client.get(
+			reverse(WORKER_DETAIL_URL, args=[self.user.pk])
+		)
+
+		self.assertEqual(response.status_code, 200)
+		self.assertEqual(
+			list(response.context["active_tasks"]),
+			[self.active_task1, self.active_task2]
+		)
+		self.assertEqual(
+			list(response.context["resolved_tasks"]),
+			[self.resolved_task1, self.resolved_task2]
+		)
+		self.assertEqual(response.context["today"], date.today())
