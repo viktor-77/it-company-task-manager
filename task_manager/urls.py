490fdd58297634b2b1c6f290f960fe52476a7403
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-19 18:38:56 +0200

    Add URL pattern for worker delete view

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from task_manager.views import (
 	index,
+	WorkerDeleteView,
 	WorkerDetailView,
 	WorkerListView,
 )
@@ -11,6 +12,11 @@ urlpatterns = [
 	path("workers/", WorkerListView.as_view(), name="worker_list"),
 	path(
 		"workers/<int:pk>/", WorkerDetailView.as_view(), name="worker_detail"
+	),
+	path(
+		"workers/delete/<int:pk>/",
+		WorkerDeleteView.as_view(),
+		name="worker_delete"
 	),
 ]
 
