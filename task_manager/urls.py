917ff71ebb14e6b5e44e9abba4fbda504216c267
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-19 02:25:19 +0200

    Add worker list URL pattern

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from task_manager.views import index
+from task_manager.views import WorkerListView, index
 
 urlpatterns = [
 	path("", index, name="index"),
+	path("workers/", WorkerListView.as_view(), name="worker_list"),
 ]
 
 app_name = "task_manager"
