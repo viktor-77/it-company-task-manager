4e868a3f15e44665dbd4d5d4d1b2ce3497d86ac2
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-20 03:49:29 +0200

    Add TaskForm with custom widgets

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from task_manager.models import Task
+
 
 class SearchForm(forms.Form):
 	query = forms.CharField(
@@ -9,3 +11,44 @@ class SearchForm(forms.Form):
 			attrs={"placeholder": "Search...", "class": "form-control"}
 		)
 	)
+
+
+class TaskForm(forms.ModelForm):
+	priority = forms.ChoiceField(
+		choices=Task.PRIORITY_CHOICES,
+		widget=forms.RadioSelect
+	)
+
+	class Meta:
+		model = Task
+		fields = [
+			"name",
+			"description",
+			"deadline",
+			"is_completed",
+			"priority",
+			"task_type",
+			"assignees",
+		]
+		widgets = {
+			"name": forms.TextInput(
+				attrs={
+					"class": "form-control",
+					"placeholder": "Enter task name"
+				}
+			),
+			"description": forms.Textarea(
+				attrs={
+					"class": "form-control",
+					"placeholder": "Enter task description"
+				}
+			),
+			"deadline": forms.DateInput(
+				attrs={"class": "form-control", "type": "date"}
+			),
+			"is_completed": forms.CheckboxInput(
+				attrs={"class": "form-check-input"}
+			),
+			"task_type": forms.RadioSelect(),
+			"assignees": forms.SelectMultiple(attrs={"class": "form-select"})
+		}
