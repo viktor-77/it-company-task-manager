23066031468b07b355dd09478a3aaf866ee9af0b
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-22 07:51:08 +0200

    Set DEBUG to False

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 INTERNAL_IPS = ["127.0.0.1"]
 
