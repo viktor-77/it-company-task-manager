d5b227712130110cdb21efc88f501ebf9e616d71
viktor-77 <vumanskiy0664041341@gmail.com>
2024-12-28 09:03:35 +0200

    Add RENDER_EXTERNAL_HOSTNAME to prod settings

--- it_company_task_manager/settings/prod.py
+++ it_company_task_manager/settings/prod.py
@@ -13,6 +13,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
+if RENDER_EXTERNAL_HOSTNAME := os.environ.get('RENDER_EXTERNAL_HOSTNAME'):
+	ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 # Application definition
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
